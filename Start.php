7b5d1f9af4e0f0eb48cb29c9357852848a75f6ae
Michael Gordon <gordonmj@users.noreply.github.com>
2014-08-13 09:42:25 -0400

    Update Start.php

--- Start.php
+++ Start.php
@@ -1,29 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 <title>Start the game</title>
 </head>
 <body>
-<h1>Welcome!</h1>
-<p>This is a simple betting game involving a deck of 54 cards (2 jokers).<br>
-Four cards will be dealt and you will place bets on the qualities of the fifth card (face and suit).<br>
-Payouts for the bets are (roughly) proportional to the probability.</p>
-<p>Creator: Michael Gordon, Adjunct Lecturer, Queens College, CUNY</p>
+<?php
+session_start();
+$suits = array (
+    " of Spades", " of Hearts", " of Clubs", " of Diamonds"
+);
+
+$faces = array (
+    2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"
+);
+
+$deck = array();
+
+foreach ($suits as $suit) {
+    foreach ($faces as $face) {
+        $deck[] = array ("face"=>$face, "suit"=>$suit);
+    }
+}
+$Joker = array("face"=>"Joker", "suit"=>"");
+$deck[] = $Joker;
+$deck[] = $Joker;
+shuffle($deck);
+
+$_SESSION['deck'] = $deck;
+/*
+foreach($deck as $card){
+  echo $card['face'] . $card['suit'] . '<br>';
+}
+*/
+?>
 <p>You start with $100.</p>
 <form action="Bet.php" method="post">
+<input type="hidden" name="cash" value=100>
 <input type="submit" value="Play!">
 </form>
-<p>
-<b>Future Improvements:</b></p>
-<ul>
-<li>Enhanced appearance with CSS</li>
-<li>Quicker and more detailed betting options</li>
-<li>List of high scores</li>
-<li>Removal of deprecating HTML tags, if any</li>
-</ul>
-</p>
-<p>The next version will probably be in JavaScript</li></p>
-<p>Feedback is welcome. If you're viewing this, you should already have my contact info. :) </p>
 </body>
 </html>
