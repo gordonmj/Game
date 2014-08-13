c8a591314dd7e85b640a130741e5e921e3dfa0ab
Michael Gordon <gordonmj@users.noreply.github.com>
2014-08-13 09:39:25 -0400

    Create Start.php

---
+++ Start.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+<title>Start the game</title>
+</head>
+<body>
+<h1>Welcome!</h1>
+<p>This is a simple betting game involving a deck of 54 cards (2 jokers).<br>
+Four cards will be dealt and you will place bets on the qualities of the fifth card (face and suit).<br>
+Payouts for the bets are (roughly) proportional to the probability.</p>
+<p>Creator: Michael Gordon, Adjunct Lecturer, Queens College, CUNY</p>
+<p>You start with $100.</p>
+<form action="Bet.php" method="post">
+<input type="submit" value="Play!">
+</form>
+<p>
+<b>Future Improvements:</b></p>
+<ul>
+<li>Enhanced appearance with CSS</li>
+<li>Quicker and more detailed betting options</li>
+<li>List of high scores</li>
+<li>Removal of deprecating HTML tags, if any</li>
+</ul>
+</p>
+<p>The next version will probably be in JavaScript</li></p>
+<p>Feedback is welcome. If you're viewing this, you should already have my contact info. :) </p>
+</body>
+</html>
