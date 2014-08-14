a90fca1ebd2aaaaf05c66546d5e6d05cfa27ffc0
Michael Gordon <gordonmj@users.noreply.github.com>
2014-08-14 01:28:25 -0400

    Update Start.php

--- Start.php
+++ Start.php
@@ -1,42 +1,61 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 <title>Start the game</title>
 </head>
 <body>
+<h1>Welcome!</h1>
+<p>This is a simple betting game involving a deck of 54 cards (2 jokers).<br>
+Four cards will be dealt and you will place bets on the qualities of the fifth card (face and suit).<br>
+Payouts for the bets are (roughly) proportional to the probability.</p>
+<p>Creator: Michael Gordon, Adjunct Lecturer, Queens College, CUNY</p>
+<p>You start with $100.</p>
 <?php
 session_start();
-$suits = array (
-    " of Spades", " of Hearts", " of Clubs", " of Diamonds"
-);
+$suits = array ("Spade", "Heart", "Diamond", "Club");
 
-$faces = array (
-    2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"
-);
+$faces = array ("Ace", "Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten", "Jack", "Queen", "King");
 
 $deck = array();
-
+$imgnum = 127137;
+$imgcode = "&#".$imgnum;
 foreach ($suits as $suit) {
     foreach ($faces as $face) {
-        $deck[] = array ("face"=>$face, "suit"=>$suit);
+        $deck[] = array ("face"=>$face, "suit"=>$suit, "img"=>$imgcode);
+        $imgcode = "&#".++$imgnum;
     }
 }
-$Joker = array("face"=>"Joker", "suit"=>"");
+$Joker = array("face"=>"Joker", "suit"=>"Joker");
 $deck[] = $Joker;
 $deck[] = $Joker;
+/*
+foreach($deck as $card){
+  $str = $card['face'] . " of " . $card['suit'] . "s " . $card['img'];
+  echo "<p style="font-family:Symbola"> $str </p>";
+}
+*/
 shuffle($deck);
 
 $_SESSION['deck'] = $deck;
-/*
-foreach($deck as $card){
-  echo $card['face'] . $card['suit'] . '<br>';
-}
-*/
+$_SESSION['cash'] = 100;
+
+
+
 ?>
-<p>You start with $100.</p>
 <form action="Bet.php" method="post">
-<input type="hidden" name="cash" value=100>
 <input type="submit" value="Play!">
 </form>
+<p>
+<b>Future Improvements:</b></p>
+<ul>
+<li>Enhanced appearance with CSS</li>
+<li>Quicker and more detailed betting options</li>
+<li>List of high scores</li>
+<li>Removal of deprecating HTML tags, if any</li>
+</ul>
+</p>
+<p>The next version will probably be in JavaScript</li></p>
+<p>Feedback is welcome. If you're viewing this, you should already have my contact info. :) </p>
 </body>
 </html>
